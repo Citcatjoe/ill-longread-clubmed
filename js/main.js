0367f0711cd9d6b4b5a520ce00c5013c48dcc874
César Greppin <Citcatjoe@users.noreply.github.com>
2019-09-19 17:18:23 +0200

    jeudi 17h

--- js/main.js
+++ js/main.js
@@ -1,65 +1,53 @@
 jQuery(document).ready(function($) 
 {
-	// INITIALISATION DU SLIDER
-	// $('.bxslider').bxSlider({
-	// 	//mode: 'fade'
-	// });
+	
+	LisbonneTL = new TimelineMax({ paused: true });
 
-	mainTl = new TimelineMax();
-	var $city = $('.lisbonne');
+	var $citySection = $('.city-section');
+	var $city = $citySection.find('.city');
 	var $cityOverlay = $city.find('.city-overlay');
 	var $cityTitle = $city.find('.city-title');
 	var $cityBullets = $city.find('li');
 	var $cityBtn = $city.find('button');
 
-	mainTl
-		.set($city, { autoAlpha: 1, height: '0px', padding: '0px', transformOrigin: "center center" })
+	clearLisbonne();
+
+	function clearLisbonne() {
+      var clearLisbonneTl = new TimelineMax();
+
+      clearLisbonneTl
+       .set($citySection, { height: '0px', transformOrigin: "center center" })
+		.set($city, { opacity: 0, transformOrigin: "center center" })
 		.set($cityTitle, { autoAlpha: 0, x: 30, transformOrigin: "center center" })
 		.set($cityBullets, { autoAlpha: 0, x: 30, transformOrigin: "center center" })
 		.set($cityBtn, { autoAlpha: 0, transformOrigin: "center center" });
+        
+      return clearLisbonneTl;
+    }
 
-	$('h3').click(function() 
-	{
-		mainTl
-			.to($city, 1.5, {height: '300px', padding: '40px', ease: Power2.easeOut})
-			.to($cityOverlay, 1, {autoAlpha: 0, ease: Power2.easeOut})
-			.to($cityTitle, 2, {autoAlpha: 1, x: 0, ease: Power4.easeOut})
-			.staggerTo($cityBullets, 2, {autoAlpha: 1, x: 0, ease: Power4.easeOut}, 0.5, '-=0.5')
-			.to($cityBtn, 2, {autoAlpha: 1, ease: Power4.easeOut}, '-=1.0');
-	});
+	var chartController = new ScrollMagic.Controller();
 
+	new ScrollMagic.Scene({
+    triggerElement: ".triggerLisbonne",
+    triggerHook: 0.5,
+    duration: 1
+ 	})
+    .offset(0) //tip top
+    .on('enter', function (e) {
+      //alert('coucou');
+      LisbonneTL.play();
+    })
+    .addIndicators()
+    .addTo(chartController);
+
+    LisbonneTL
+		.to($citySection, 0.75, {height: '400px', paddingTop: '50px', paddingBottom: '50px', ease: Power2.easeOut})
+		.to($city, 0.75, {opacity: 1, ease: Power2.easeOut}, '-=0.3')
+		.to($cityOverlay, 0.5, {autoAlpha: 0, ease: Power2.easeOut}, '-=0.5')
+		.to($cityTitle, 1, {autoAlpha: 1, x: 0, ease: Power4.easeOut})
+		.staggerTo($cityBullets, 1, {autoAlpha: 1, x: 0, ease: Power4.easeOut}, 0.5, '-=0.25')
+		.to($cityBtn, 1, {autoAlpha: 1, ease: Power4.easeOut}, '-=0.5');
 	
-		// .to($sideContainer, transitionTiming, {width: 0, ease: Power4.easeOut, className:"-=is-visible"}, '-=' + transitionTiming);
-
-	// var transitionTiming = 1;
-
-	// mainTl = new TimelineMax();
-	// var $mapContainer = $('.map-container');
-	// var $sideContainer = $('.side-container');
-
-	// $mapContainer.click(function() 
-	// {
-	// 	if($sideContainer.hasClass('is-visible'))
-	// 	{
-	// 		mainTl
-	// 			.to($mapContainer, transitionTiming, {width: '100%', ease: Power4.easeOut, className:"-=is-visible"})
-	// 			.to($sideContainer, transitionTiming, {width: 0, ease: Power4.easeOut, className:"-=is-visible"}, '-=' + transitionTiming);
-	// 	}
-	// 	else
-	// 	{
-	// 		mainTl
-	// 			.to($mapContainer, transitionTiming, {width: '50%', ease: Power4.easeOut, className:"-=is-visible"})
-	// 			.to($sideContainer, transitionTiming, {width: '50%', ease: Power4.easeOut, className:"+=is-visible"}, '-=' + transitionTiming);
-	// 	}
-	// });
-
-	 // $('.city').tilt({
-	 // 		maxTilt: 10,
-	 // 		perspective: 500,
-	 // 		speed: 1500,
-  //           glare: false,
-  //           maxGlare: .2
-  //       });
 
 
 
